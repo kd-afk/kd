3d9ac28d8f2301266db81733879f6c8979e74b72
kd-afk <rajputkuldeepsingh936@gmail.com>
2024-09-04 00:54:17 +0530

    added jquery validation and profile page to update data

--- kd02/app.js
+++ kd02/app.js
@@ -8,6 +8,8 @@ const port = 3000;
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/public", express.static("public"));
+app.use('/uploads', express.static('uploads'));
+
 
 // Use the auth routes
 app.use(authRoutes);
--- kd02/routes/auth.js
+++ kd02/routes/auth.js
@@ -7,7 +7,8 @@ const router = express.Router();
 const { sql, poolPromise } = require("../db/db");
 const ensureAuthenticated = require("./authMiddleware");
 const { body, validationResult } = require('express-validator');
-
+const multer = require('multer');
+const path = require('path');
 
 const secret = crypto.randomBytes(64).toString('hex');
 router.use(session({
@@ -36,25 +37,27 @@ router.post("/signup", [
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  const { username, email, password , fname, lname, mNumber, address} = req.body;
+  else{
+    const { username, email, password , fname, lname, mNumber, address} = req.body;
+    
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const pool = await poolPromise;
+      const request = pool.request();
+      request.input("username", sql.NVarChar, username);
+      request.input("email", sql.NVarChar, email);
+      request.input("password", sql.NVarChar, hashedPassword);
+      request.input("fname", sql.NVarChar, fname);
+      request.input("lname", sql.NVarChar, lname);
+      request.input("mNumber", sql.NVarChar, mNumber);
+      request.input("address", sql.NVarChar, address);
   
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const pool = await poolPromise;
-    const request = pool.request();
-    request.input("username", sql.NVarChar, username);
-    request.input("email", sql.NVarChar, email);
-    request.input("password", sql.NVarChar, hashedPassword);
-    request.input("fname", sql.NVarChar, fname);
-    request.input("lname", sql.NVarChar, lname);
-    request.input("mNumber", sql.NVarChar, mNumber);
-    request.input("address", sql.NVarChar, address);
-
-    await request.execute("spCreateUser");
-    res.render("login");
-  } catch (err) {
-    console.error("Error during signup:", err);
-    res.render("signup", { message: "Error during signup. Please try again." });
+      await request.execute("spCreateUser");
+      res.render("login");
+    } catch (err) {
+      console.error("Error during signup:", err);
+      res.render("signup", { message: "Error during signup. Please try again." });
+    }
   }
 });
 
@@ -115,64 +118,58 @@ router.post("/forgot-password", async (req, res) => {
   try {
     const pool = await poolPromise;
     const request = pool.request();
-    request.input("email", sql.NVarChar, email);
 
-    const result = await request.query(
-      "SELECT * FROM users WHERE email = @email"
-    );
-    if (result.recordset.length === 0) {
+    const token = crypto.randomBytes(20).toString("hex");
+
+    const expires = new Date();
+    expires.setHours(expires.getHours() + 8);
+
+    request.input("email", sql.NVarChar, email);
+    request.input("token", sql.NVarChar, token);
+    request.input("expires", sql.DateTime, expires);
+    
+    const result = await request.execute('RequestPasswordReset');
+    
+
+    if (result.recordset[0].Result === 0) {
       return res.render("forgot-password", {
         message: "No account found with that email.",
       });
     }
+    else{
+      //const user = result.recordset[0];
+  
+      // Send an email with the reset link
+      const transporter = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+          user: "rajputkuldeepsingh936@gmail.com",
+          pass: "xzvm fndp mfle zxwg",
+        },
+      });
+  
+      const mailOptions = {
+        to: email,
+        from: "rajputkuldeepsingh936@gmail.com",
+        subject: "Password Reset",
+        text:
+          `You are receiving this because you have requested the reset of the password for your account.\n\n` +
+          `Please click on the following link, or paste this into your browser to complete the process:\n\n` +
+          `http://${req.headers.host}/reset-password/${token}\n\n` +
+          `If you did not request this, please ignore this email and your password will remain unchanged.\n`,
+      };
+  
+      await transporter.sendMail(mailOptions);
+  
+      res.render("forgot-password", {
+        message:
+          "An email has been sent to " +
+          email +
+          " with further instructions.",
+      });
 
-    const user = result.recordset[0];
-    // Generate a reset token
-    const token = crypto.randomBytes(20).toString("hex");
+    }
 
-    // Set token expiration time (e.g., 1 hour)
-    const expires = new Date();
-    expires.setHours(expires.getHours() + 8);
-
-    // Store the token and expiration in the database
-    const updateQuery = `
-            UPDATE users 
-            SET resetPasswordToken = @token, resetPasswordExpires = @expires 
-            WHERE email = @email
-        `;
-    request.input("token", sql.NVarChar, token);
-    request.input("expires", sql.DateTime, expires);
-
-    await request.query(updateQuery);
-
-    // Send an email with the reset link
-    const transporter = nodemailer.createTransport({
-      service: "Gmail",
-      auth: {
-        user: "rajputkuldeepsingh936@gmail.com",
-        pass: "xzvm fndp mfle zxwg",
-      },
-    });
-
-    const mailOptions = {
-      to: user.email,
-      from: "rajputkuldeepsingh936@gmail.com",
-      subject: "Password Reset",
-      text:
-        `You are receiving this because you have requested the reset of the password for your account.\n\n` +
-        `Please click on the following link, or paste this into your browser to complete the process:\n\n` +
-        `http://${req.headers.host}/reset-password/${token}\n\n` +
-        `If you did not request this, please ignore this email and your password will remain unchanged.\n`,
-    };
-
-    await transporter.sendMail(mailOptions);
-
-    res.render("forgot-password", {
-      message:
-        "An email has been sent to " +
-        user.email +
-        " with further instructions.",
-    });
   } catch (err) {
     console.error("Error during password reset request:", err);
     res.render("forgot-password", {
@@ -184,31 +181,31 @@ router.post("/forgot-password", async (req, res) => {
 
 router.get("/reset-password/:token", async (req, res) => {
   const { token } = req.params;
+  res.render("reset-password", { token });
+  
+  // try {
+  //   const pool = await poolPromise;
+  //   const request = pool.request();
 
-  try {
-    const pool = await poolPromise;
-    const request = pool.request();
-    request.input("token", sql.NVarChar, token);
+  //   request.input("token", sql.NVarChar, token);
 
-    const result = await request.query(
-      "SELECT * FROM users WHERE resetPasswordToken = @token AND resetPasswordExpires > GETDATE()"
-    );
+  //   const result = await request.execute('ResetPassword');
 
-    if (result.recordset.length === 0) {
-      return res.render("reset-password", {
-        message: "Password reset token is invalid or has expired.",
-        token,
-      });
-    }
+  //   if (result.recordset[0].Result === 0) {
+  //     return res.render("reset-password", {
+  //       message: "Password reset token is invalid or has expired.",
+  //       token,
+  //     });
+  //   }else{
+  //   }
 
-    res.render("reset-password", { token });
-  } catch (err) {
-    console.error("Error during password reset:", err);
-    res.render("reset-password", {
-      message:
-        "An error occurred during the password reset process. Please try again.",
-    });
-  }
+  // } catch (err) {
+  //   console.error("Error during password reset:", err);
+  //   res.render("reset-password", {
+  //     message:
+  //       "An error occurred during the password reset process. Please try again.",
+  //   });
+  // }
 });
 
 router.post("/reset-password/:token", async (req, res) => {
@@ -218,29 +215,21 @@ router.post("/reset-password/:token", async (req, res) => {
   try {
     const pool = await poolPromise;
     const request = pool.request();
+
     request.input("token", sql.NVarChar, token);
 
-    const result = await request.query(
-      "SELECT * FROM users WHERE resetPasswordToken = @token AND resetPasswordExpires > GETDATE()"
-    );
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    if (result.recordset.length === 0) {
+    request.input("hashed_password", sql.NVarChar, hashedPassword);
+
+    const result = await request.execute('ResetPassword');
+
+    if (result.recordset[0].Result === 0) {
       return res.render("reset-password", {
         message: "Password reset token is invalid or has expired.",
         token,
       });
     }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const updateQuery = `
-            UPDATE users 
-            SET password = @hashed_password, resetPasswordToken = NULL, resetPasswordExpires = NULL 
-            WHERE resetPasswordToken = @token
-        `;
-    request.input("hashed_password", sql.NVarChar, hashedPassword);
-
-    await request.query(updateQuery);
 
     res.render("login", {
       message:
@@ -263,6 +252,89 @@ router.get('/logout', (req, res) => {
       }
       res.redirect('/login');
   });
+});
+
+router.get('/check-username', async (req, res) => {
+  const { username } = req.query;
+
+  try {
+      const pool = await poolPromise;
+      const request = pool.request();
+
+      request.input('username', sql.NVarChar, username);
+
+      // Execute the stored procedure
+      const result = await request.execute('CheckUsernameAvailability');
+      const userCount = result.recordset[0].UserCount;
+
+      if (userCount > 0) {
+          return res.json({ available: false });
+      } else {
+          return res.json({ available: true });
+      }
+  } catch (err) {
+      console.error('Error during username availability check:', err);
+      return res.status(500).json({ available: false, error: 'Server error' });
+  }
+});
+
+
+// Configure Multer for file uploads (profile picture)
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+      cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+      cb(null, req.session.user.id + path.extname(file.originalname)); // Use user ID for filename
+  }
+});
+
+const upload = multer({ storage: storage });
+
+
+// Route to render the user profile page
+router.get('/profile',ensureAuthenticated, async (req, res) => {
+  try {
+      // console.log(req.user)
+      // const pool = await poolPromise;
+      // const request = pool.request();
+      // request.input('id', sql.Int, req.user.id); // Assuming user ID is stored in session
+
+      // // Execute the stored procedure
+      // const result = await request.execute('GetUserProfile');
+      // const user = result.recordset[0];
+
+      res.render('profile', { user: req.session.user});
+  } catch (err) {
+      console.error('Error fetching user data:', err);
+      res.status(500).send('Server error');
+  }
+});
+// Route to handle profile updates
+router.post('/profile', upload.single('profilePicture'), async (req, res) => {
+  const { firstName, lastName, email, mobile, address } = req.body;
+  const profilePicture = req.file ? req.file.filename : req.body.existingProfilePicture;
+  user = req.session.user
+
+  try {
+      const pool = await poolPromise;
+      const request = pool.request();
+      request.input('id', sql.Int, user.id);
+      request.input('fname', sql.NVarChar, firstName);
+      request.input('lname', sql.NVarChar, lastName);
+      request.input('email', sql.NVarChar, email);
+      request.input('mNumber', sql.NVarChar, mobile);
+      request.input('address', sql.NVarChar, address);
+      request.input('profilePicture', sql.NVarChar, profilePicture);
+
+      // Execute the stored procedure
+      await request.execute('UpdateUserProfile');
+
+      res.redirect('/dashboard');
+  } catch (err) {
+      console.error('Error updating user profile:', err);
+      res.status(500).send('Server error');
+  }
 });
 
 
